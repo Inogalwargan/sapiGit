29fbd0eaea78c8106009969a66b1010c6b83e04e
chris <christiandwi13@gmail.com>
2018-11-02 11:01:26 +0700

    benerin web.php

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,8 @@ Route::get('tambahPengajuan', 'c_inti@tambahPengajuan');
 
 Route::get('tambahSapi', 'c_inti@tambahSapi');
 
-<<<<<<< HEAD
+
 Route::get('lihatPengajuan', 'c_inti@lihatPengajuan');
-=======
 Route::get('/pabrik/beranda', 'c_pabrik@beranda');
 Route::get('/pabrik/tambah_bahan', 'c_pabrik@tambah_bahan_baku');
 Route::get('/pabrik/lihat_bahan', 'c_pabrik@lihat_bahan_baku');
@@ -38,4 +37,3 @@ Route::get('pakan', 'c_inti@pakan');
 
 Route::get('addCicilan', 'c_inti@addCicilan');
 
->>>>>>> e84ad221a4cde17be517090fdf8158827f278d49
