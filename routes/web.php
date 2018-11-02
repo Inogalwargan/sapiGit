e9292723cc5f6494b170e6e7bdc42f2b43a6defa
Muhamat Abdul Rohim <Muhamatabdulrohim@gmail.com>
2018-11-02 09:05:26 +0700

    Update routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,16 @@ Route::get('homeInti', 'c_inti@homeInti');
 Route::get('tambahPengajuan', 'c_inti@tambahPengajuan');
 
 Route::get('tambahSapi', 'c_inti@tambahSapi');
+
+Route::get('/pabrik/beranda', 'c_pabrik@beranda');
+Route::get('/pabrik/tambah_bahan', 'c_pabrik@tambah_bahan_baku');
+Route::get('/pabrik/lihat_bahan', 'c_pabrik@lihat_bahan_baku');
+
+Route::get('homePeternak', 'c_peternak@homePeternak');
+
+Route::get('pelunasan', 'c_inti@pelunasan');
+
+Route::get('pakan', 'c_inti@pakan');
+
+Route::get('addCicilan', 'c_inti@addCicilan');
+
