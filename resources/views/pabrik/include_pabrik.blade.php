39046bc049b097f6558cc288fb38577a3074a9e1
Muhamat Abdul Rohim <Muhamatabdulrohim@gmail.com>
2018-11-06 06:41:06 +0700

    Update resources/views/pabrik/include_pabrik.blade.php

--- resources/views/pabrik/include_pabrik.blade.php
+++ resources/views/pabrik/include_pabrik.blade.php
@@ -1,61 +1,55 @@
-  <div class="navigation-menu">
+<div class="navigation-menu">
 
-        <ul class="menu-items custom-scroll">
-            <li>
-                <a href="/pabrik/beranda">
-                    <span class="icon-thumbnail"><i class="dripicons-browser"></i></span>
-                    <span class="title">Beranda</span>
-                </a>
-            </li>
-            <li>
-                <a href="javascript:void(0);" class="have-submenu">
-                    <span class="icon-thumbnail"><i class="dripicons-document-new"></i></span>
-                    <span class="title">Bahan Baku</span>
-                </a>
-                <!--Submenu-->
-                <ul class="sub-menu">
-                    <li>
-                        <a href="/pabrik/tambah_bahan">
-                            <span class="icon-thumbnail"><i class="fi-plus"></i></span>
-                            <span class="title">Tambah</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/pabrik/beli_bahan">
-                            <span class="icon-thumbnail"><i class="fi-dollar"></i></span>
-                            <span class="title">Beli</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/pabrik/lihat_bahan">
-                            <span class="icon-thumbnail"><i class="fi-page-filled"></i></span>
-                            <span class="title">Lihat</span>
-                        </a>
-                    </li>
-                   
-                </ul>
-            </li>
-            <li>
-                <a href="javascript:void(0);" class="have-submenu">
-                    <span class="icon-thumbnail"><i class="dripicons-document-new"></i></span>
-                    <span class="title">Pakan</span>
-                </a>
-                <!--Submenu-->
-                <ul class="sub-menu">
-                    <li>
-                        <a href="/tambahPakan">
-                            <span class="icon-thumbnail"><i class="fi-plus"></i></span>
-                            <span class="title">Tambah Pakan</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/lihatPakan">
-                            <span class="icon-thumbnail"><i class="fi-page-filled"></i></span>
-                            <span class="title">Lihat Pakan</span>
-                        </a>
-                    </li>
-                   
-                </ul>
-            </li>
-        </ul>
-    </div>
+    <ul class="menu-items custom-scroll">
+        <li>
+            <a href="/pabrik/beranda">
+                <span class="icon-thumbnail"><i class="dripicons-browser"></i></span>
+                <span class="title">Beranda</span>
+            </a>
+        </li>
+        <li>
+            <a href="javascript:void(0);" class="have-submenu">
+                <span class="icon-thumbnail"><i class="dripicons-document-new"></i></span>
+                <span class="title">Bahan Baku</span>
+            </a>
+            <!--Submenu-->
+            <ul class="sub-menu">
+                <li>
+                    <a href="/pabrik/beli_bahan">
+                        <span class="icon-thumbnail"><i class="fi-dollar"></i></span>
+                        <span class="title">Beli</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="/pabrik/lihat_bahan">
+                        <span class="icon-thumbnail"><i class="fi-page-filled"></i></span>
+                        <span class="title">Lihat</span>
+                    </a>
+                </li>
+
+            </ul>
+        </li>
+        <li>
+            <a href="javascript:void(0);" class="have-submenu">
+                <span class="icon-thumbnail"><i class="dripicons-document-new"></i></span>
+                <span class="title">Pakan</span>
+            </a>
+            <!--Submenu-->
+            <ul class="sub-menu">
+                <li>
+                    <a href="/tambahPakan">
+                        <span class="icon-thumbnail"><i class="fi-plus"></i></span>
+                        <span class="title">Tambah Pakan</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="/lihatPakan">
+                        <span class="icon-thumbnail"><i class="fi-page-filled"></i></span>
+                        <span class="title">Lihat Pakan</span>
+                    </a>
+                </li>
+
+            </ul>
+        </li>
+    </ul>
+</div>
