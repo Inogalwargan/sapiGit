0ea7581c8a37dd08d9eafe609888d5a3ae3d9c5b
ino <marianogalwargan@gmail.com>
2018-11-07 17:10:07 +0700

    Update resources/views/inti/v_home.blade.php

--- resources/views/inti/v_home.blade.php
+++ resources/views/inti/v_home.blade.php
@@ -126,7 +126,7 @@
                 <div class="row">
                     <div class="col-9 col-md-6 d-lg-none">
                         <a id="toggle-navigation" href="javascript:void(0);" class="icon-btn mr-3"><i class="fa fa-bars"></i></a>
-                        <span class="logo">WOW - Admin</span>
+                        <span class="logo">SAPI BALURAN</span>
                     </div>
                     <div class="col-lg-8 d-none d-lg-block">
                         <ol class="breadcrumb">
